7ab3c9ba54ec3667457f9df019d536845bd1038d
Emir Salman <emirsalman.info@gmail.com>
2025-08-16 21:58:33 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>home</h1>,
+  },
+  {
+    path: "/about",
+    element: <h1>about</h1>,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
