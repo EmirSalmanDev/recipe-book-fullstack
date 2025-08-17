c06c41857d79d68e6f5da96d04b28d027fdbb6ea
Emir Salman <emirsalman.info@gmail.com>
2025-08-17 20:08:16 +0300

    Add styled-components setup for practicing CSS-in-JS

--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 // Landing
+import styled from "styled-components";
+
+const StyledBtn = styled.button`
+  font-size: 1.5rem;
+  background: red;
+  color: white;
+`;
 
 const Testing = () => {
-  return <h1>Testing Page</h1>;
+  return (
+    <div>
+      <h1>Testing Page</h1>
+      <StyledBtn>styled button</StyledBtn>
+    </div>
+  );
 };
 
 export default Testing;
