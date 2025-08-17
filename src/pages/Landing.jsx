97ee0e971b83055feaa1c1f4f5f36d73c487b5ab
Emir Salman <emirsalman.info@gmail.com>
2025-08-17 21:59:38 +0300

    Add logo component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/LandingPage";
-import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 import { Link } from "react-router-dom";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="recipe" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
