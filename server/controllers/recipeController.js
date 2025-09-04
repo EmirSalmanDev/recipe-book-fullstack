57887f6b97f370a74fc849ec6b8d7076aab7c61c
Emir Salman <emirsalman.info@gmail.com>
2025-09-04 13:47:39 +0300

    Add search query to backend

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -10,9 +10,43 @@ export const getRecipeById = async (req, res) => {
 };
 
 export const getAllRecipes = async (req, res) => {
-  const recipes = await Recipe.find({ createdBy: req.user.userId }); // attached by jwt
+  const { search, recipeStatus, sort } = req.query;
 
-  res.status(StatusCodes.OK).json({ recipes });
+  const queryObject = {
+    createdBy: req.user.userId, // attached by jwt
+  };
+  if (search) {
+    queryObject.$or = [{ title: { $regex: search, $options: "i" } }]; // mongoDB query operator
+  }
+  if (recipeStatus && recipeStatus !== "all") {
+    queryObject.recipeStatus = recipeStatus;
+  }
+
+  const sortOptions = {
+    newest: "-createdAt",
+    oldest: "createdAt",
+    "a-z": "title",
+    "z-a": "-title",
+  };
+
+  const sortKey = sortOptions[sort] || sortOptions.newest;
+
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const recipes = await Recipe.find(queryObject)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+
+  const totalRecipes = await Recipe.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalRecipes / limit);
+
+  res
+    .status(StatusCodes.OK)
+    .json({ totalRecipes, numOfPages, currentPage: page, recipes });
 };
 
 export const createRecipe = async (req, res) => {
